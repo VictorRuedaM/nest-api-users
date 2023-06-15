3c4da4ed38e69d7fa78eda755e54f76841ddf737
Victor Rueda <vialruma1985@gmail.com>
2023-06-15 18:54:31 -0500

    getUserById and getUserByName added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,32 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create.user.dto';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number): Promise<User> {
+    return this.userService.getUserByID(id);
+  }
+  @Get('/name/:name')
+  getUserBiName(@Param('name') name: string): Promise<User> {
+    return this.userService.getUserByName(name);
+  }
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.userService.getUsers();
+  }
   @Post()
   createUser(@Body() newUser: CreateUserDto) {
     return this.userService.createUser(newUser);
-  }
-
-  @Get()
-  getUsers() {
-    return this.userService.getUsers();
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,6 +7,20 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
+  getUserByID(id: number): Promise<User> {
+    return this.userRepository.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+  getUserByName(name: string): Promise<User> {
+    return this.userRepository.findOne({
+      where: {
+        userName: name,
+      },
+    });
+  }
 
   getUsers(): Promise<User[]> {
     return this.userRepository.find();
